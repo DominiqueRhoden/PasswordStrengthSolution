be8d434720956c7ec0b2826a2812d71b2c2cb0bc
DominiqueRhoden <drho4706@students.vsu.edu>
2025-09-15 15:20:52 -0400

    Add XML documentation to GenerateV4Uuid method

--- PasswordStrength/UuidGenerator.cs
+++ PasswordStrength/UuidGenerator.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 
 public static class UuidGenerator
-{
-    /// <summary>
-    /// Generates a valid version 4 UUID (random).
+{   /// <summary>
+    /// Generates a valid version 4 UUID.
+    /// A version 4 UUID is randomly generated and follows the format:
+    /// xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
+    /// where '4' indicates version 4, and 'y' is one of 8, 9, A, or B.
     /// </summary>
-    /// <returns>A new UUID string in the format xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx.</returns>
+    /// <returns>
+    /// A string containing a version 4 UUID (e.g., "3f2504e0-4f89-41d3-9a0c-0305e82c3301").
+    /// </returns>
+
     public static string GenerateV4Uuid()
     {
         return Guid.NewGuid().ToString();
