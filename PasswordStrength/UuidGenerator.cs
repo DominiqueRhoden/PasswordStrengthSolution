60183d0099fae9b9987b0dfb3b25225930e0976f
DominiqueRhoden <drho4706@students.vsu.edu>
2025-09-15 15:13:45 -0400

    Add GenerateV4Uuid method

---
+++ PasswordStrength/UuidGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class UuidGenerator
+{
+    /// <summary>
+    /// Generates a valid version 4 UUID (random).
+    /// </summary>
+    /// <returns>A new UUID string in the format xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx.</returns>
+    public static string GenerateV4Uuid()
+    {
+        return Guid.NewGuid().ToString();
+    }
+}
