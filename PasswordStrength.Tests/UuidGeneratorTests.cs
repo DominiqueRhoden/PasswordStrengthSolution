e7a1b4a7afe998903eda0767f01610c684c7f5cc
DominiqueRhoden <drho4706@students.vsu.edu>
2025-09-15 15:17:22 -0400

    Add unit tests for GenerateV4Uuid method

---
+++ PasswordStrength.Tests/UuidGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using System;
+using System.Text.RegularExpressions;
+
+public class UuidGeneratorTests
+{
+    [Fact]
+    public void GenerateV4Uuid_ShouldReturnValidV4Uuid()
+    {
+        // Arrange
+        var uuid = UuidGenerator.GenerateV4Uuid();
+
+        // Assert: matches UUID format
+        Assert.Matches(@"^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$", uuid);
+
+        // Assert: parses successfully
+        Assert.True(Guid.TryParse(uuid, out _));
+    }
+}
